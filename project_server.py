34c095e54ab25ebefe1baac339255323eae97df8
bradhoward1 <bradley.howard@duke.edu>
2020-06-22 17:05:07 -0400

    Definition of Patient class for database

---
+++ project_server.py
@@ -0,0 +1,24 @@
+# project_server.py
+
+from flask import Flask, request, jsonify
+from datetime import datetime
+from pymodm import connect, MongoModel, fields
+
+# connect("mongodb+srv://brad_howard:NA@cluster0-lucsp.mongodb.net/random_db?retryWrites=true&w=majority")
+
+
+# if __name__ == '__main__':
+#     u = Patient(mr_number=123, name="Brad",nums=[9])
+#     u.save()
+#     bob=Patient.objects.raw({"_id": 123})
+#     bob.update({ "$push": { "nums": { "$each": [ 90, 92, 85, 20 ] } } })
+#     ####pymodm.random_db.update({ "$push": { nums: { "$each":[90,92,85]}}})
+#     ####bob_user.save()
+
+class Patient(MongoModel):
+    mr_number = fields.IntegerField(primary_key=True)
+    name = fields.CharField()
+    heart_rates = fields.ListField()
+    medical_image = fields.ImageField()
+    ECG_image = fields.ImageField()
+    datetimes = fields.ListField()
