13855f99dd4cc47585c95b33ee027c9fe2629fca
bradhoward1 <bradley.howard@duke.edu>
2020-06-23 16:36:54 -0400

    Client testing for posting to the server

--- client_tests.py
+++ client_tests.py
@@ -21,10 +21,47 @@ r = requests.post("http://127.0.0.1:5000/add_new_patient", json=out)
 print("{}, {}".format(r.text, r.status_code))
 
 
-out = {"medical_record_number": 101,
+out = {"medical_record_number": 102,
        "patient_name": "Brad",
        "medical_image": "jpeg_image",
        "heart_rate": 90,
        "ECG_image": "second_jpeg_image"}
 r = requests.post("http://127.0.0.1:5000/add_new_patient", json=out)
 print("{}, {}".format(r.text, r.status_code))
+
+
+out = {"medical_record_number": 102,
+       "patient_name": "Brad",
+       "medical_image": "jpeg_image",
+       "heart_rate": 90,
+       "ECG_image": "second_jpeg_image"}
+r = requests.post("http://127.0.0.1:5000/add_new_patient", json=out)
+print("{}, {}".format(r.text, r.status_code))
+
+
+out = {"medical_record_number": 102,
+       "patient_name": "Brad",
+       "medical_image": "jpeg_image",
+       "heart_rate": 90,
+       "ECG_image": "second_jpeg_image"}
+r = requests.post("http://127.0.0.1:5000/add_new_patient", json=out)
+print("{}, {}".format(r.text, r.status_code))
+
+
+out = {"medical_record_number": 102,
+       "patient_name": "Brad",
+       "medical_image": "jpeg_image",
+       "heart_rate": 90,
+       "ECG_image": "second_jpeg_image"}
+r = requests.post("http://127.0.0.1:5000/add_new_patient", json=out)
+print("{}, {}".format(r.text, r.status_code))
+
+
+out = {"medical_record_number": 102,
+       "patient_name": "Brad",
+       "medical_image": "jpeg_image",
+       "heart_rate": 90,
+       "ECG_image": "second_jpeg_image"}
+r = requests.post("http://127.0.0.1:5000/add_new_patient", json=out)
+print("{}, {}".format(r.text, r.status_code))
+
